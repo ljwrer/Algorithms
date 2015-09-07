97156bb890ebe57ea4b3b6d46283f9d4a1813d3d
ljwrer <ljwrer@sina.com>
2015-09-07 15:57:29 +0800

    Sort

--- js/main.js
+++ js/main.js
@@ -8,6 +8,6 @@ require.config({
 require(["search","sort"],function(search,sort){
 	var arr1 = [5, 98, 76, 25, 47, 34, 87, 14, 12];
 	var arr2 = [1, 2, 5, 9, 15, 25, 35, 66, 78, 97];
-	alert(search.binary(arr2,15));
-	alert(sort.quickSort(arr1))
+//	alert(search.binary(arr2,15));
+	alert(sort.insertionSort(arr1))
 });
--- js/sort.js
+++ js/sort.js
@@ -8,18 +8,54 @@ define([], function() {
 		for (var i = 0; i < arr.length; i++) {
 			if (arr[i] < pivotItem) {
 				left.push(arr[i]);
-			}
-			if (arr[i] > pivotItem) {
+			} else {
 				right.push(arr[i]);
 			}
 		}
 		return quickSort(left).concat([pivotItem], quickSort(right));
 	}
-	function bubbleSort(){
-				
+	function bubbleSort(arr) {
+		for (var i = 0; i < arr.length-1; i++) {
+			for (var j = 0; j < arr.length-i-1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					arr[j] = arr[j] ^ arr[j + 1];
+					arr[j + 1] = arr[j] ^ arr[j + 1];
+					arr[j] = arr[j] ^ arr[j + 1];
+				}
+			}
+		}
+		return arr;
+	}
+	function  selectionSort(arr){
+		var min,temp;
+		for(var i=0;i<arr.length-1;i++){
+			min=i;
+			for(var j=i+1;j<arr.length;j++){
+				if(arr[j]<arr[min]){
+					min=j;
+				}
+			}
+			temp=arr[i];
+			arr[i]=arr[min];
+			arr[min]=temp;
+		}
+		return arr;
+	}
+	function insertionSort(arr){
+		var temp,j;
+		for(var i=1;i<arr.length;i++){
+			temp=arr[i]
+			for(j=i;j>0&&arr[j-1]>temp;j--){
+				arr[j]=arr[j-1];
+			}
+			arr[j]=temp;
+		}
+		return arr;
 	}
 	return {
-		quickSort:quickSort,
-		bubbleSort:bubbleSort
+		quickSort: quickSort,
+		bubbleSort: bubbleSort,
+		selectionSort:selectionSort,
+		insertionSort:insertionSort
 	}
 })
